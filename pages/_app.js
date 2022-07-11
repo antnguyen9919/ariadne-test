5040ee1dd016ba81e6fb3959ca078922caddb3b2
ge34bec <an.nguyen@tum.de>
2022-07-11 13:38:52 +0200

    modified google analytics to lazy loading

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout'
 import {AnimatePresence} from "framer-motion"
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
+// import { useEffect } from 'react';
+// import TagManager from 'react-gtm-module';
 
 import Script from 'next/script'
 
@@ -11,21 +11,21 @@ import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-TLNNDCF' });
-  }, []);
+  // useEffect(() => {
+  //   TagManager.initialize({ gtmId: 'GTM-TLNNDCF' });
+  // }, []);
 
   return(
    
      <>
-    <Script strategy="lazyOnload" src = {`https://www.googletagmanager.com/gtag/js?id=G-DY5ZJ1KV4Y`} />
+    <Script strategy="afterInteractive" src = {`https://www.googletagmanager.com/gtag/js?id=GTM-TLNNDCF`} />
 
-    <Script strategy="lazyOnload"  id="Google Tag Manager">
+    <Script strategy="afterInteractive"  id="Google Tag Manager">
       {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-DY5ZJ1KV4Y');`}
+  gtag('config', 'GTM-TLNNDCF');`}
     </Script>   
 
 <Layout>
