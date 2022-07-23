7215fdf21e8ff1d1a00e5e5f7fcc4bf17b1e2434
ge34bec <an.nguyen@tum.de>
2022-07-23 02:48:12 +0200

    last sitemap fix

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -60,6 +60,8 @@ export const getServerSideProps = async ({ res }) => {
   //     }
   //     return `${baseUrl}/${staticPagePath}`;
   //   });
+
+  //   console.log(staticPages)
      
 
   //   const industries = fs
@@ -117,8 +119,77 @@ export const getServerSideProps = async ({ res }) => {
   //   });
     
     const allPages = [ 
-      // ...staticPages,...solutions,...industries, ...company, 
-      ...dynamic, `${baseUrl}/blog/index.js`,`${baseUrl}/company/index.js`]
+      `${baseUrl}/`,
+      `${baseUrl}/why-ariadne`,
+      `${baseUrl}/solutions/ariadne-analytics`,
+      `${baseUrl}/solutions/ariadne-engagement`,
+      `${baseUrl}/solutions/ariadne-navigation`,
+      `${baseUrl}/solutions/data-lifecycle`,
+      `${baseUrl}/industries/airports`,
+      `${baseUrl}/industries/cities`,
+      `${baseUrl}/industries/hospitality`,
+      `${baseUrl}/industries/retail-stores`,
+      `${baseUrl}/industries/shopping-centers`,
+      `${baseUrl}/industries/transportation`,
+      `${baseUrl}/company/about-us`,
+      `${baseUrl}/company/contact`,
+      `${baseUrl}/company/disclaimer`,
+      `${baseUrl}/company/privacy`,
+      `${baseUrl}/company/careers/business-development-jobs`,
+      `${baseUrl}/company/careers/data-science-jobs`,
+      `${baseUrl}/company/careers/social-media-jobs`,
+      `${baseUrl}/letstalk`,
+        //de
+        `${baseUrl}/de`,
+      `${baseUrl}/de/why-ariadne`,
+      `${baseUrl}/de/solutions/ariadne-analytics`,
+      `${baseUrl}/de/solutions/ariadne-engagement`,
+      `${baseUrl}/de/solutions/ariadne-navigation`,
+      `${baseUrl}/de/solutions/data-lifecycle`,
+      `${baseUrl}/de/industries/airports`,
+      `${baseUrl}/de/industries/cities`,
+      `${baseUrl}/de/industries/hospitality`,
+      `${baseUrl}/de/industries/retail-stores`,
+      `${baseUrl}/de/industries/shopping-centers`,
+      `${baseUrl}/de/industries/transportation`,
+      `${baseUrl}/de/company/about-us`,
+      `${baseUrl}/de/company/contact`,
+      `${baseUrl}/de/company/disclaimer`,
+      `${baseUrl}/de/company/privacy`,
+      `${baseUrl}/de/company/careers/business-development-jobs`,
+      `${baseUrl}/de/company/careers/data-science-jobs`,
+      `${baseUrl}/de/company/careers/social-media-jobs`,
+      `${baseUrl}/de/letstalk`,
+
+      //fr
+      `${baseUrl}/fr`,
+      `${baseUrl}/fr/why-ariadne`,
+      `${baseUrl}/fr/solutions/ariadne-analytics`,
+      `${baseUrl}/fr/solutions/ariadne-engagement`,
+      `${baseUrl}/fr/solutions/ariadne-navigation`,
+      `${baseUrl}/fr/solutions/data-lifecycle`,
+      `${baseUrl}/fr/industries/airports`,
+      `${baseUrl}/fr/industries/cities`,
+      `${baseUrl}/fr/industries/hospitality`,
+      `${baseUrl}/fr/industries/retail-stores`,
+      `${baseUrl}/fr/industries/shopping-centers`,
+      `${baseUrl}/fr/industries/transportation`,
+      `${baseUrl}/fr/company/about-us`,
+      `${baseUrl}/fr/company/contact`,
+      `${baseUrl}/fr/company/disclaimer`,
+      `${baseUrl}/fr/company/privacy`,
+      `${baseUrl}/fr/company/careers/business-development-jobs`,
+      `${baseUrl}/fr/company/careers/data-science-jobs`,
+      `${baseUrl}/fr/company/careers/social-media-jobs`,
+      `${baseUrl}/fr/letstalk`,
+
+      , `${baseUrl}/blog`,`${baseUrl}/company/careers`,
+      `${baseUrl}/de/blog`,`${baseUrl}/de/company/careers`,
+      `${baseUrl}/fr/blog`,`${baseUrl}/fr/company/careers`,
+
+
+     
+      ...dynamic]
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
