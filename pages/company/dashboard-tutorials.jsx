3c983d15d40c15c6a921a82a70ce5007f2401376
An T Nguyen <andyngn@Ans-MacBook-Pro.local>
2022-08-01 08:59:35 +0200

    tutorials

---
+++ pages/company/dashboard-tutorials.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import cs from "../../styles/Tutorials.module.css";
+import { links } from "./tutorial-links";
+import { useState } from "react";
+const DashboardTutorial = () => {
+  const [video, setVideo] = useState(0);
+
+  const videoHandler = (e) => {
+    setVideo(e.target.id);
+  };
+  return (
+    <div className={cs.container}>
+      <div className={cs.flexbox}>
+        <div className={cs.vidcell}>
+          <ReactPlayer
+            // style={{ position: "absolute" }}
+            url={links[video].url}
+            controls
+            width='100%'
+            height='100%'
+            stopOnUnmount={true}
+          />
+        </div>
+        <div className={cs.bar}>
+          <div className='drop-shadow-md'>
+            <h1 className='text-center mb-2'>DashboardTutorial</h1>
+          </div>
+
+          {links.map((link) => (
+            <ol type='1'>
+              <li
+                className={
+                  video == link.id
+                    ? "bg-slate-200 font-bold"
+                    : "hover:bg-slate-200"
+                }
+                key={link.id}
+              >
+                {link.id + 1}. <span> </span>
+                <button onClick={(e) => videoHandler(e)}>
+                  <p id={link.id}> {link.title}</p>
+                </button>
+              </li>
+            </ol>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardTutorial;
---
+++ pages/company/tutorial-links.js
@@ -0,0 +1,82 @@
+export const links = [
+  {
+    id: 0,
+    title: "How to place devices ",
+    url: "https://www.youtube.com/watch?v=GWGvfWMxZ24",
+  },
+  {
+    id: 1,
+    title: "How to remove devices ",
+    url: "https://www.youtube.com/watch?v=qjh36GXVlfU",
+  },
+  {
+    id: 2,
+    title: "How to change the device names ",
+    url: "https://www.youtube.com/watch?v=Mo49YHx7EyM",
+  },
+  {
+    id: 3,
+    title: "How to change device locations ",
+    url: "https://www.youtube.com/watch?v=s9UlV6m1eH8 ",
+  },
+  {
+    id: 4,
+    title: "How to create a floor plan ",
+    url: "https://www.youtube.com/watch?v=WlqY-F_mJ0A ",
+  },
+  {
+    id: 5,
+    title: "How to obtain manual counting ",
+    url: "https://www.youtube.com/watch?v=zDoKqXpqwPs ",
+  },
+  {
+    id: 6,
+    title: "How to select a certain area ",
+    url: "https://www.youtube.com/watch?v=YmeqdTIlPss ",
+  },
+  {
+    id: 7,
+    title: "How to change the area of interest ",
+    url: "https://www.youtube.com/watch?v=hJuOmXwA2mQ ",
+  },
+  {
+    id: 8,
+    title: "How to change the name of an area ",
+    url: "https://www.youtube.com/watch?v=a0k-VsCmdws ",
+  },
+  {
+    id: 9,
+    title: "Panel customization - min, max, mean… ",
+    url: "https://www.youtube.com/watch?v=hfUd0pbEt3Y ",
+  },
+  {
+    id: 10,
+    title: "How to enable bars, different displays, intervals? ",
+    url: "https://www.youtube.com/watch?v=BkqlNNXfn2w ",
+  },
+  {
+    id: 11,
+    title: "How to place devices ",
+    url: "https://www.youtube.com/watch?v=hR9s1FFsCBo ",
+  },
+  {
+    id: 12,
+    title: "How to enable bars, different displays, intervals? ",
+    url: "https://www.youtube.com/watch?v=Nv4zC7TkNfw ",
+  },
+  {
+    id: 13,
+    title: "How to place devices ",
+    url: "https://www.youtube.com/watch?v=fptaT7hAblY",
+  },
+  {
+    id: 14,
+    title: "How to place devices ",
+    url: "https://www.youtube.com/watch?v=TWMRFQaGwG4",
+  },
+  {
+    id: 15,
+    title: "How to change thresholds in transitions panel ",
+    url: "https://www.youtube.com/watch?v=KYV_fdui6XU",
+  },
+];
